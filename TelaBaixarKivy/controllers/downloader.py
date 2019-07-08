9b1c1475d5957fd7268f382d6874e1f0eaf425c8
Guilherme <guilhermenepomuceno46@gmail.com>
2019-07-08 08:40:27 -0300

    Confirmação de Saida Botoes Renomear TODO: Implementar Renomear

--- TelaBaixarKivy/controllers/downloader.py
+++ TelaBaixarKivy/controllers/downloader.py
@@ -1,6 +1,11 @@
 """."""
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.popup import Popup
+
 from .telaBase import Tela  # pylint: disable=relative-beyond-top-level
-# from .popup import PopupDownload  # pylint: disable=relative-beyond-top-level
 
 
 class Downloader(Tela):
@@ -9,6 +14,26 @@ class Downloader(Tela):
     def on_enter(self, *args, **kwargs):
         """."""
         super().on_enter(*args, **kwargs)
-        # p = PopupDownload()
-        # p.open()
-        # p.baixar("https://www.superanimes.site/cartoon/a-formiga-atomica/51653")
+
+    def on_pre_enter(self):
+        """Executa antes de Entrar na Tela."""
+        Window.bind(on_request_close=self.confirmacao)
+
+    def confirmacao(self, *args, **kwargs):
+        """Popup de Confirmação de Saida."""
+        box = BoxLayout(orientation='vertical', padding=10, spacing=10)
+        botoes = BoxLayout(padding=10, spacing=10)
+
+        pop = Popup(title='Deseja mesmo sair?', content=box, size_hint=(None, None),
+                    size=(300, 130))
+
+        sim = Button(text='Sim', on_release=App.get_running_app().stop)
+        nao = Button(text='Não', on_release=pop.dismiss)
+
+        botoes.add_widget(sim)
+        botoes.add_widget(nao)
+
+        box.add_widget(botoes)
+
+        pop.open()
+        return True
